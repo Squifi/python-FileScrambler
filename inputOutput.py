6dade30505d965557d373dc56809bb9af3605435
squifi <squifi@entwicklerfreu.de>
2016-08-01 20:42:40 +0200

    Formatted Text, minor changes and added a todo

--- inputOutput.py
+++ inputOutput.py
@@ -1,14 +1,20 @@
+# TODO: 2016-08-01, add the possibility to render an entire directory useless :)
+
 import hashlib
+
 filename='file.txt'
+
 with open(filename, 'r') as file:
     read_data = file.read()
 read_data = read_data.encode('utf-8')
 
 codedString = hashlib.sha512(read_data)
+
 print("Files contents were encrypted.")
 
 filename=filename[0:5] + 'fkd'
 with open(filename, 'wb+') as file:
     file.write(codedString.digest())
+
 print("File operation completed contents encrypted")
 print("Programme ended.")
